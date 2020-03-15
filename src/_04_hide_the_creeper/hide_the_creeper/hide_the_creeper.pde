73bba3634bc1abcb03c791418aee9f84d638c1a6
root <root@d82f63fe42bf>
2020-03-15 19:58:01 +0000

    calculator

--- src/_04_hide_the_creeper/hide_the_creeper/hide_the_creeper.pde
+++ src/_04_hide_the_creeper/hide_the_creeper/hide_the_creeper.pde
@@ -1,19 +1,22 @@
 PImage creeper;     //at the top of your program
 
+
+
 void setup() {
 
   size(1200, 850);
-  PImage minecraft = loadImage("steal.png");     //in setup method
-minecraft.resize(width, height);          //in setup method
-background(minecraft);          //in setup method
-  
+          //in setup method
+ // minecraft.resize(width, height);          //in setup method
+     //in setup method
 creeper=loadImage("creeper.png");     //in setup method
-
+ PImage minecraft = loadImage("steal.png"); 
 creeper.resize(55, 55);     //in setup method
+minecraft.resize (width, height); 
+background (minecraft); 
 }
 
 void draw() {
-
+ //PImage minecraft = loadImage("steal.png"); 
 image(creeper, mouseX, mouseY);     //in draw method  
-
+ //background (minecraft); 
 }
--- src/_05_change_calculator/ChangeCalculator.java
+++ src/_05_change_calculator/ChangeCalculator.java
@@ -15,16 +15,21 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String input = JOptionPane.showInputDialog("How many nickels do u have");
 		// Convert their answer to an int using Integer.parseInt()
-
+int yay = Integer.parseInt(input);
+yay *= 5;
 		// Ask the user how many dimes they have, and convert their answer
-
+String input2 = JOptionPane.showInputDialog("How many dimes do u have");
 		// Ask the user how many quarters they have, and convert their answer
-
+int oklol = Integer.parseInt(input);
+oklol *= 10;
 		// Calculate how much money the user has and save it in a double variable 
-
+String input3 = JOptionPane.showInputDialog("how many quarters do u have");
+int oi = Integer.parseInt(input); 
+oi *= 25; 
 		// Tell the user how much money they have
+JOptionPane.showMessageDialog(null,  yay + oi + oklol);
 
 	}
 }
